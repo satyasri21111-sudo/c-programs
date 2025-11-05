00a8bb430cfbfd9e4f3387415e792784c7b04cbb
satyasri21111-sudo <satyasri21111@gmail.com>
2025-11-05 19:23:02 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=20,b=10;
+	printf("a is less then b:%d",a<b);     //relational operators
+	printf("a is greater then b:%d",a>b);
+	printf("a is equal then b:%d",a==b);
+	printf("a is greater than or equal to b then b:%d",a
+	>=b);
+	printf("a is less than or equal to b then b:%d",a<=b);
+	printf("a is not equal to b then b:%d",a!=b);
+	return 0;
+}
