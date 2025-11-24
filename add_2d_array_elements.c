746ed850a4883a6b46de8122b97a07b3576227c9
satyasri21111-sudo <satyasri21111@gmail.com>
2025-11-24 22:25:15 +0530

    Add files via upload

---
+++ add_2d_array_elements.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],b[3][3],i,j,sum[3][3];
+	printf("enter first matrix elements");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("enter second matrix elements");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+			
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			sum[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	printf("sum of two matrices:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",sum[i][j]);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+	
+	
+}
